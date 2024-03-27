67c1a20e0b23ce0737e7afd7d7d9658dfe8724d9
j4m3s 4l4r1c <65734218+JamesAlaric@users.noreply.github.com>
2024-03-27 17:19:39 +0100

    [Feat] Adding Font Feature

---
+++ src/app/font.ts
@@ -0,0 +1,9 @@
+import { Advent_Pro, Montserrat } from 'next/font/google'
+ 
+ 
+export const mts = Montserrat({
+        style: ['normal', 'italic'],
+        subsets: ['latin'],
+        display: 'swap',
+        variable: '--font-mts'
+})
